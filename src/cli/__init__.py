eb999731b9818bf8703150a5402b52022c6a5282
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2026-09-25 18:30:01 +0300

    feat: add cli scaffold

---
+++ src/cli/__init__.py
@@ -0,0 +1,3 @@
+"""Command line tools for exploring a running RASENMAEHER deployment"""
+
+__version__ = "0.0.1"
---
+++ src/cli/__main__.py
@@ -0,0 +1,6 @@
+"""python -m cli"""
+
+from cli.console import rmcli
+
+if __name__ == "__main__":
+    rmcli()
